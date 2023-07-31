974d96eb50fae48b74d7059e9b0ed406cf3ea93b
Nikita Obergan <Obergannikita@gmail.com>
2023-07-31 11:04:03 -0400

    Add class Day

---
+++ LemonadeStand/Day.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    internal class Day
+    {
+        internal Weather weather;
+        internal List<Customer> customers;
+        public Day()
+        {
+            weather = new Weather();
+            customers = new List<Customer>();
+        }
+    }
+}
