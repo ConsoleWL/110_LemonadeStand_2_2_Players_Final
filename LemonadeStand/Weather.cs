429e0142dcf0e6e78c1d022f5a745a2c74bf05f3
Nikita Obergan <Obergannikita@gmail.com>
2023-07-31 10:53:28 -0400

    Add class Weather

---
+++ LemonadeStand/Weather.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    internal class Weather
+    {
+        public string condition;
+        public int temperature;
+        public List<string> weatherConditions;
+        public string predictedForecast;
+        public bool isWeatherChanged = false;
+
+        public Weather()
+        {
+            weatherConditions = new List<string>
+            {
+                "Hot. Sunny. Clear Sky",
+                "Warm. Sunny. Cloudy",
+                "Cold. No sun. Cloudy"
+            };
+        }
+    }
+}
